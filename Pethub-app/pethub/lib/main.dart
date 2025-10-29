5d46fd453bf1833e79ded85ff295c8818a5e8fec
HernanMCHR <nomaslimites@gmail.com>
2025-10-28 23:28:12 -0300

    -DNM- Metodo para no cerrar sesion
    No cierra sesion al cerrar la app

--- Pethub-app/pethub/lib/main.dart
+++ Pethub-app/pethub/lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screens/login_page.dart'; // Importa la página de login
-import 'utils/app_colors.dart'; // Importa la paleta de colores
+import 'screens/login_page.dart';
+import 'utils/app_colors.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'main_shell.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // <-- esta línea es clave
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -20,33 +20,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, 
+      debugShowCheckedModeBanner: false,
       title: 'PetHub',
-      
       theme: ThemeData(
         useMaterial3: true,
         primaryColor: AppColors.primary,
-
         // ¡AQUÍ ESTÁ LA CORRECCIÓN DEL COLOR!
         // Esta línea cambia el fondo de AMARILLO a BLANCO/GRIS.
-        scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255), 
-
+        scaffoldBackgroundColor: const Color.fromARGB(255, 255, 255, 255),
         // Tema global para todos los AppBars
         appBarTheme: const AppBarTheme(
           backgroundColor: AppColors.primary,
-          foregroundColor: AppColors.textLight, // Color de íconos y texto
+          foregroundColor: AppColors.textLight,
           elevation: 0,
         ),
-        
         textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: AppColors.primary,
-          ),
+          style: TextButton.styleFrom(foregroundColor: AppColors.primary),
         ),
       ),
-      
-      home: const LoginPage(),
+      home: const _DeciderPage(), //se decide Login o MainShell
     );
   }
 }
 
+class _DeciderPage extends StatelessWidget {
+  const _DeciderPage();
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null && user.emailVerified) {
+      return const MainShell();
+    }
+    return const LoginPage();
+  }
+}
