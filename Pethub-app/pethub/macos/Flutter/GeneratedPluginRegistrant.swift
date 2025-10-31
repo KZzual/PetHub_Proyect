24e1f31f6339afe3c2ac9447f4d6f068bc9fbc13
HernanMCHR <nomaslimites@gmail.com>
2025-10-31 13:21:24 -0300

    update

--- Pethub-app/pethub/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Pethub-app/pethub/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,15 +9,14 @@ import cloud_firestore
 import firebase_auth
 import firebase_core
 import firebase_storage
-import shared_preferences_foundation
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
